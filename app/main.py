c1d3306a2ea1dc7b86cd80782e532fc7bfd95b8d
eric9816 <eric981623@gmail.com>
2023-09-07 21:12:31 +0300

    завершение фронтенда

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ from app.hotels.router import router as router_hotels
 from app.pages.router import router as router_pages
 from app.images.router import router as router_image
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 # монтирование - добавляем отдельного приложение
@@ -18,6 +20,21 @@ app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_pages)
 app.include_router(router_image)
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # чтобы с фронта куки могли приходить на бек и мы могли бы авторизовать его
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # if __name__ == "__main__":
 #     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
